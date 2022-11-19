c16be358bfb457d69b3b417d0792ead8dde1aa43
Dmitrii Obraztsov <xa12850498@student.karazin.ua>
2022-11-19 14:52:31 +0200

    Update

--- LabWork3.rb
+++ LabWork3.rb
@@ -1,36 +1,23 @@
 require 'benchmark'
-=begin
-puts "Введіть параметри моделі"
 
 count_ns = 0
 thickness = 0.0
 pa = 0.0
 kr = 0.0
 mu = 0.0
-=end
-hade = 45.0
-thickness = 2.0
-count_ns = 10000
-
-mu = 1.0
-pa = 0.001
-kr = 1.0
+hade = 0.0
 
 =begin
-while 1
-  begin
-    puts "Статистика (Ns):"
-    count_ns = Integer(STDIN.gets.chomp)
+hade = 45.0
+thickness = 2.0
+count_ns = 5
 
-    if count_ns > 0
-      break
-    else
-      puts "Введено від'ємне значення. Повторіть вибір"
-    end
-  rescue ArgumentError
-    puts "Введено не ціле значення. Повторіть вибір"
-  end
-end
+mu = 1.0
+pa = 0.01
+kr = 1.0
+=end
+
+puts "Характеристики випромінювання та шару речовини\n\n"
 
 while 1
   begin
@@ -40,7 +27,7 @@ while 1
     if thickness > 0
       break
     else
-      puts "Введено від'ємне значення. Повторіть вибір"
+      puts "Значення поза допустимим діапазоном. Повторіть вибір"
     end
   rescue ArgumentError
     puts "Не коректне значення. Повторіть вибір"
@@ -52,17 +39,17 @@ while 1
     puts "Кут падіння:"
     hade = Float(STDIN.gets.chomp)
 
-    if hade > 0
+    if hade >= 0 and hade <= 360
       break
     else
-      puts "Введено від'ємне значення. Повторіть вибір"
+      puts "Значення поза допустимим діапазоном. Повторіть вибір"
     end
   rescue ArgumentError
-    puts "Введено не ціле значення. Повторіть вибір"
+    puts "Не коректне значення. Повторіть вибір"
   end
 end
 
-puts "\nВведіть параметри взаємодії\n\n"
+puts "\nХарактеристики процесу взаємодії частинок із речовиною\n\n"
 
 while 1
   begin
@@ -81,13 +68,13 @@ end
 
 while 1
   begin
-    puts "Параметр розкиду:"
+    puts "Модельний параметр індикатриси розсіювання:"
     kr = Float(STDIN.gets.chomp)
 
     if kr >= 0
       break
     else
-      puts "Введено від'ємне значення. Повторіть вибір"
+      puts "Значення поза допустимим діапазоном. Повторіть вибір"
     end
   rescue ArgumentError
     puts "Не коректне значення. Повторіть вибір"
@@ -103,81 +90,106 @@ while 1
       mu = 1 / mu
       break
     else
-      puts "Введено від'ємне значення. Повторіть вибір"
+      puts "Значення поза допустимим діапазоном. Повторіть вибір"
     end
   rescue ArgumentError
     puts "Не коректне значення. Повторіть вибір"
   end
 end
-=end
+
+puts "\nПараметр методу Монте-Карло\n\n"
+
+while 1
+  begin
+    puts "Статистика (Ns):"
+    count_ns = Integer(STDIN.gets.chomp)
+
+    if count_ns > 0
+      break
+    else
+      puts "Значення поза допустимим діапазоном. Повторіть вибір"
+    end
+  rescue ArgumentError
+    puts "Введено не ціле значення. Повторіть вибір"
+  end
+end
 
 def calculation(count_ns, thickness, pa, kr, mu, hade)
   f_var = 0.0
   b_var = 0.0
   ab_var = 0.0
 
-  (1..count_ns).each do
-    x = 0.0
-    cs = Math.cos(Math::PI * hade / 180)
-    while 1
-      l = -Math.log(rand) / mu
-      x1 = x + l * cs
+  start_measurement = Time.now.to_f
 
-      if x1 < 0
+  (1..count_ns).each do
+    x = 0.0 # координата поверхні шару, що опромінюється
+
+    if hade == 90.0 or hade == 270.0
+      #cs = 6.123031769111886e-17
+      cs = 0.0
+    else
+      cs = Math.cos(Math::PI * hade / 180.0) # значення кута в радіанах
+    end
+    while 1
+      l = -Math.log(rand(0.0..1.0)) / mu # формула 1.1
+      x1 = x + l * cs # формула 1.2 та 1.3 (через перезапис значень на кожній ітерації)
+
+      if x1 < 0 # умова виходу "назад"
         b_var += 1
         break
       end
 
-      if x1 > thickness
+      if x1 > thickness # умова виходу "вперед"
         f_var += 1
         break
       end
 
-      if rand(0.0..1.0) < pa
+      r = rand(0.0..1.0) # умова поглинання (формула 1.4)
+      if r < pa
         ab_var += 1
         break
       end
 
-      com = rand(0.0..1.0) ** (1 / (kr + 1))
-      fi = 2 * Math::PI * rand(0.0..1.0)
-      cos_1 = cs * com - Math.sqrt((1 - cs * cs) * (1 - com * com)) * Math.sin(fi) # за формулою cos^2 + sin^2 = 1
+      com = rand(0.0..1.0) ** (1 / (kr + 1)) # формула 1.6
+      fi = 2 * Math::PI * rand(0.0..1.0) # формула 1.7
+      cos_1 = cs * com - Math.sqrt((1 - cs * cs) * (1 - com * com)) * Math.sin(fi) # за формулою cos^2 + sin^2 = 1; використовується формула 1.8
 
       x = x1
       cs = cos_1
     end
   end
 
-  q_var1 = (f_var.to_f / count_ns.to_f).round(4)
-  q_var2 = (b_var.to_f / count_ns.to_f).round(4)
-  q_var3 = (ab_var.to_f / count_ns.to_f).round(4)
+  end_measurement = Time.now.to_f
 
+  q_var1 = (f_var.to_f / count_ns.to_f).round(4) # ймовірність виходу частинки "вперед"
+  q_var2 = (b_var.to_f / count_ns.to_f).round(4) # ймовірність виходу частинки "назад"
+  q_var3 = (ab_var.to_f / count_ns.to_f).round(4) # ймовірність поглинання частинки
+
+  # розрахунок похибок
   f_ns = Math.sqrt(q_var1 * (1 - q_var1).abs / count_ns.to_f).round(5)
   b_ns = Math.sqrt(q_var2 * (1 - q_var2).abs / count_ns.to_f).round(5)
   ab_ns = Math.sqrt(q_var3 * (1 - q_var3).abs / count_ns.to_f).round(5)
 
+  # хеш отриманих значень
   {
     :Q_F => q_var1,
     :Q_B => q_var2,
     :Q_Ab => q_var3,
     :F_Ns => f_ns,
     :B_Ns => b_ns,
-    :Ab_Ns => ab_ns
+    :Ab_Ns => ab_ns,
+    :time_start => start_measurement,
+    :time_end => end_measurement
   }
 end
 
-puts "Результати моделювання"
+puts "\nРезультати моделювання\n\n"
 results = calculation(count_ns, thickness, pa, kr, mu, hade)
-q_calc = (results.dig(:Q_F) + results.dig(:Q_B) + results.dig(:Q_Ab) / 3) * Benchmark.measure {calculation(count_ns, thickness, pa, kr, mu, hade)}.real / count_ns
+dispersion_qf = results.dig(:Q_F) * (1 - results.dig(:Q_F))
+dispersion_qb = results.dig(:Q_B) * (1 - results.dig(:Q_B))
+dispersion_qab = results.dig(:Q_Ab) * (1 - results.dig(:Q_Ab))
+laboriousness = ((dispersion_qf + dispersion_qb + dispersion_qab) / 3.0) * ((results.dig(:time_end) - results.dig(:time_start)) / 3.0)
 puts("Передавальне відношення: #{results.dig(:Q_F)} :::: Похибка: #{results.dig(:F_Ns)}")
 puts("Коефіцієнт відбиття:     #{results.dig(:Q_B)} :::: Похибка: #{results.dig(:B_Ns)}")
 puts("Коефіцієнт поглинання:   #{results.dig(:Q_Ab)} :::: Похибка: #{results.dig(:Ab_Ns)}")
-puts("Трудомісткість:          #{q_calc}")
-=begin
-puts("Test(Q_f): #{results.dig(:Q_F)}")
-puts("Test(Q_b): #{results.dig(:Q_B)}")
-puts("Test(Q_ab): #{results.dig(:Q_Ab)}")
-
-puts("Test(F_Ns): #{results.dig(:F_Ns)}")
-puts("Test(B_Ns): #{results.dig(:B_Ns)}")
-puts("Test(Ab_Ns): #{results.dig(:Ab_Ns)}")
-=end
+puts("Трудомісткість:          #{laboriousness} с.")
